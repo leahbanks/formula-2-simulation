58910209293ebec1b0aed51a7739c9c201711776
Leah <leahb_96@hotmail.co.uk>
2023-06-02 20:00:40 +0100

    added circuit data

--- src/data/races.js
+++ src/data/races.js
@@ -4,83 +4,97 @@ export const races = [
     country: "Bahrain",
     date: "03 - 05 Mar",
     img: "assets/flags/bahrain.png",
+    circuit: "Bahrain International Circuit"
   },
   {
     name: "JEDDAH",
     country: "Saudi Arabia",
     date: "17 - 19 Mar",
     img: "assets/flags/saudi.png",
+    circuit: "Jeddah Corniche Circuit"
   },
   {
     name: "MELBOURNE",
     country: "Australia",
     date: "31 - 02 Apr",
     img: "assets/flags/australia.png",
+    circuit: "Albert Park Circuit"
   },
   {
     name: "BAKU",
     country: "Azerbaijan",
     date: "28 - 30 Apr",
     img: "assets/flags/azerbaijan.png",
+    circuit: "Baku City Circuit"
   },
   {
     name: "IMOLA",
     country: "Italy",
     date: "19 - 21 May",
     img: "assets/flags/italy.png",
+    circuit: "Autodromo Enzo e Dino Ferrari"
   },
   {
     name: "MONTE CARLO",
     country: "Monaco",
     date: "25 - 28 May",
     img: "assets/flags/monaco.png",
+    circuit: "Circuit de Monaco"
   },
   {
     name: "BARCELONA",
     country: "Spain",
     date: "02 - 04 Jun",
     img: "assets/flags/spain.png",
+    circuit: "Circuit de Barcelona-Catalunya"
   },
   {
     name: "SPIELBERG",
     country: "Austria",
     date: "30 - 02 Jul",
     img: "assets/flags/austria.png",
+    circuit: "Red Bull Ring"
   },
   {
     name: "SILVERSTONE",
     country: "United Kingdom",
     date: "30 - 02 Jul",
     img: "assets/flags/uk.png",
+    circuit: "Silverstone Circuit"
   },
   {
     name: "BUDAPEST",
     country: "Hungary",
     date: "21 - 23 Jul",
     img: "assets/flags/hungary.png",
+    circuit: "Hungaroring"
   },
   {
     name: "SPA-FRANCORCHAMPS",
     country: "Belgium",
     date: "28 - 30 Jul",
     img: "assets/flags/belgium.png",
+    circuit: "Circuit de Spa-Francorchamps"
   },
   {
     name: "ZANDVOORT",
     country: "Netherlands",
     date: "25 - 27 Aug",
     img: "assets/flags/netherlands.png",
+    circuit: "Zandvoort Circuit"
   },
   {
     name: "MONZA",
     country: "Italy",
     date: "01 - 03 Sept",
     img: "assets/flags/italy.png",
+    circuit: "Autodromo Nazionale Monza"
   },
   {
     name: "YAS ISLAND",
     country: "United Arab Emirates",
     date: "24 - 26 Nov",
     img: "assets/flags/uae.png",
+    circuit: "Yas Marina Circuit"
   },
 ];
