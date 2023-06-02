55da55286c600a26dc6fc44833ae866f7e161930
Leah <leahb_96@hotmail.co.uk>
2023-06-02 19:16:21 +0100

    carousel of races complete

--- src/components/Header/Calendar/Calendar.jsx
+++ src/components/Header/Calendar/Calendar.jsx
@@ -1,5 +1,6 @@
 import "./Calendar.css";
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import { races } from "../../../data/races";
 
 export default function Calendar() {
   return (
@@ -7,7 +8,25 @@ export default function Calendar() {
       <h1 className="title">
         SEASON CALENDAR <span>FORMULA 2 2023</span>
       </h1>
-      <h2 className="scroll-heading"><AiOutlineLeft className="arrow"/> SCROLL TO SEE MORE RACES <AiOutlineRight className="arrow"/></h2>
+      <h2 className="scroll-heading">
+        <AiOutlineLeft className="arrow" /> SCROLL TO SEE MORE RACES{" "}
+        <AiOutlineRight className="arrow" />
+      </h2>
+      <ol>
+        <div className="races-container">
+          {races.map((race) => (
+              <a className="races" href="">
+              <img
+                className="flag"
+                src={race.img}
+                alt={`flag of ${race.country}`}
+              />
+              <h1 className="race-name">{race.name}</h1>
+              <p className="dates">{race.date}</p>
+              </a>
+          ))}
+        </div>
+      </ol>
     </section>
   );
 }
