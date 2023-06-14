fa4f41b79f5843e78aaeefea7ee1c621a36d5cb7
Leah <leahb_96@hotmail.co.uk>
2023-06-14 12:55:40 +0100

    added route to race pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Header from './components/Header/Header'
-import Calendar from './components/Calendar/Calendar';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Calendar from "./components/Calendar/Calendar";
+import { Route, Routes } from "react-router-dom";
+import { RaceInfo } from "./components/RaceInfo/RaceInfo";
 
 function App() {
   return (
     <div className="App">
-  <Header/>
-  <Calendar/>
+      <Header />
+      <Calendar/>
+      <Routes>
+        {" "}
+          <Route
+            path=":race_id"
+            element={<RaceInfo/>}
+          ></Route>;
+      </Routes>
+      
     </div>
   );
 }
