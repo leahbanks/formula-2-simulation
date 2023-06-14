33f39820199be2c258ba166b13aa58d60d04ef52
Leah <leahb_96@hotmail.co.uk>
2023-06-14 12:58:09 +0100

    removed unecessary extra class

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,7 +1,7 @@
 import "./Calendar.css";
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 import { races } from "../../data/races";
-
+import { Link } from "react-router-dom";
 
 export default function Calendar() {
   return (
@@ -16,15 +16,22 @@ export default function Calendar() {
       <ol>
         <div className="races-container">
           {races.map((race) => (
-              <a className="races" href="">
-              <img
-                className="flag"
-                src={race.img}
-                alt={`flag of ${race.country}`}
-              />
-              <h1 className="race-name">{race.name}</h1>
-              <p className="dates">{race.date}</p>
-              </a>
+            <Link
+              to={`/${race.id}`}
+              style={{ textDecoration: "none" }}
+              className="races"
+              key={race.id}
+            >
+              <div>
+                <img
+                  className="flag"
+                  src={race.img}
+                  alt={`flag of ${race.country}`}
+                />
+                <h1 className="race-name">{race.name}</h1>
+                <p className="dates">{race.date}</p>
+              </div>
+            </Link>
           ))}
         </div>
       </ol>
