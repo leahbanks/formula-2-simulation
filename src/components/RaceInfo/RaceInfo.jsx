368235fd78be5ed90178c92eac59b1e3afd64525
Leah <leahb_96@hotmail.co.uk>
2023-06-19 12:03:41 +0100

    started to add and style circuit stats

--- src/components/RaceInfo/RaceInfo.jsx
+++ src/components/RaceInfo/RaceInfo.jsx
@@ -27,39 +27,64 @@ export const RaceInfo = () => {
                 alt={`flag of ${currentRace.country}`}
               />{" "}
               <span>{currentRace.country}</span>
-            
-            <div className="city-and-round">
-              <p className="city-name">{currentRace.name}</p>
-              <p className="round-date">
-                Round {currentRace.id} | {currentRace.date}
-              </p>
+              <div className="city-and-round">
+                <p className="city-name">{currentRace.name}</p>
+                <p className="round-date">
+                  ROUND {currentRace.id} | {currentRace.date}
+                </p>
+              </div>
             </div>
+
+            <div className="race-results">
+              <h3 className="standings-title">
+                {" "}
+                ROUND {currentRace.id} SUMMARY
+              </h3>
+              <ol className="grid" style={{ listStyle: "none" }}>
+                <li>FREE PRACTICE</li>
+                <span>
+                  {currentRace.round1Summary[0].freePractice[0]}{" "}
+                  {currentRace.round1Summary[0].freePractice[1]}
+                </span>
+                <li>QUALIFYING SESSION</li>
+                <span>
+                  {currentRace.round1Summary[1].qualifyingSession[0]}{" "}
+                  {currentRace.round1Summary[1].qualifyingSession[1]}
+                </span>
+                <li>SPRINT RACE</li>
+                <span>
+                  {currentRace.round1Summary[2].sprintRace[0]}{" "}
+                  {currentRace.round1Summary[2].sprintRace[1]}
+                </span>
+                <li>FEATURE RACE</li>
+                <span>
+                  {currentRace.round1Summary[3].featureRace[0]}{" "}
+                  {currentRace.round1Summary[3].featureRace[1]}
+                </span>
+              </ol>
             </div>
-          
-          <div className="race-results">
-          <h3 className="standings-title"> ROUND {currentRace.id} SUMMARY</h3>
-            <ol className="grid" style={{listStyle: "none"}}>
-            <li>FREE PRACTICE</li>
-            <span>{currentRace.round1Summary[0].freePractice[0]} {currentRace.round1Summary[0].freePractice[1]}</span>
-            <li>QUALIFYING SESSION</li>
-            <span>{currentRace.round1Summary[1].qualifyingSession[0]} {currentRace.round1Summary[1].qualifyingSession[1]}</span>
-            <li>SPRINT RACE</li>
-            <span>{currentRace.round1Summary[2].sprintRace[0]} {currentRace.round1Summary[2].sprintRace[1]}</span>
-            <li>FEATURE RACE</li>
-            <span>{currentRace.round1Summary[3].featureRace[0]} {currentRace.round1Summary[3].featureRace[1]}</span>
-            </ol>
-          </div>
-          <div className="standings">
-          <h3 className="standings-title">STANDINGS AFTER ROUND {currentRace.id}</h3>
-          <ol className="grid-2" style={{listStyle: "none"}}>
-            <li> 1ST</li>
-            <span>{currentRace.standings[0].first[0]} {currentRace.standings[0].first[1]}</span>
-            <li>2ND</li>
-            <span>{currentRace.standings[1].second[0]} {currentRace.standings[1].second[1]}</span>
-            <li>3RD</li>
-            <span>{currentRace.standings[2].third[0]} {currentRace.standings[2].third[1]}</span>
-            </ol>
-          </div>
+            <div className="standings">
+              <h3 className="standings-title">
+                STANDINGS AFTER ROUND {currentRace.id}
+              </h3>
+              <ol className="grid-2" style={{ listStyle: "none" }}>
+                <li> 1ST</li>
+                <span>
+                  {currentRace.standings[0].first[0]}{" "}
+                  {currentRace.standings[0].first[1]}
+                </span>
+                <li>2ND</li>
+                <span>
+                  {currentRace.standings[1].second[0]}{" "}
+                  {currentRace.standings[1].second[1]}
+                </span>
+                <li>3RD</li>
+                <span>
+                  {currentRace.standings[2].third[0]}{" "}
+                  {currentRace.standings[2].third[1]}
+                </span>
+              </ol>
+            </div>
           </div>
         </div>
       </section>
@@ -72,14 +97,39 @@ export const RaceInfo = () => {
           }}
         >
           <h1>{currentRace.circuit}</h1>
-          <span className="circuit stats">
+          <div className="circuit-details">
             <img
               className="circuit-map"
               style={{ justifyContent: "left" }}
               src={currentRace.circuitMap}
               alt={`map of ${currentRace.circuit}`}
             />
-          </span>
+            <div className="circuit-stats">
+              <h4>
+                First F2 Race <p>{currentRace.firstRace}</p>
+              </h4>
+              <h4>
+                Circuit Length <p>{currentRace.circuitLength}</p>
+              </h4>
+              <h4>
+                Sprint Race <p>{currentRace.sprintRace[0]}</p>
+                <p>{currentRace.sprintRace[1]}</p>
+              </h4>
+              <h4>
+                Feature Race <p>{currentRace.featureRace[0]}</p>{" "}
+                <p>{currentRace.featureRace[1]}</p>
+              </h4>
+              <h3>
+                Circuit Records <h4>F2 Fastest Lap</h4>{" "}
+                <p>{currentRace.circuitRecords[0].f2FastestLap[0]}</p>{" "}
+                <p>
+                  {currentRace.circuitRecords[0].f2FastestLap[1]} •{" "}
+                  {currentRace.circuitRecords[0].driver}
+                </p>
+                <p>{currentRace.circuitRecords[0].date}</p>
+              </h3>
+            </div>
+          </div>
         </div>
       </section>
     </section>
